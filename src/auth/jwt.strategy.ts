1c23e7e3f1236bf16d534161513bdcf49420fb2c
Pedro Furlan <pedrofurlan6@gmail.com>
2024-01-28 17:57:45 -0300

    feat: create jwt strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { z } from 'zod';
+
+const tokenPayloadSchema = z.object({
+  sub: z.string().uuid(),
+});
+
+export type UserPayload = z.infer<typeof tokenPayloadSchema>;
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'Your secret key here',
+    });
+  }
+
+  async validate(payload: UserPayload) {
+    return tokenPayloadSchema.parse(payload);
+  }
+}
